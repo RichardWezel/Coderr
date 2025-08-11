0d8f2d9f95cf0b6ea6fba41aa68d5c4caefac3f3
RichyClean <149680604+RichyClean@users.noreply.github.com>
2025-08-11 18:10:15 +0200

    first commit, added auth_app with UserModel and first installations

---
+++ auth_app/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class CustomUser(AbstractUser):
+    class Roles(models.TextChoices):
+        CUSTOMER = "customer", "Customer"
+        BUSINESS = "business", "Business"
+
+    type = models.CharField(
+        max_length=20,
+        choices=Roles.choices,
+        default=Roles.CUSTOMER,
+    )
+
+    def __str__(self):
+        return f"{self.username} ({self.role})"
+    
+    class Meta:
+        verbose_name = "User"
+        verbose_name_plural = "Users"
+        ordering = ["username"]  
