d7976600c58021123eaf7f4b7bd7dfaa0b565c76
RichyClean <149680604+RichyClean@users.noreply.github.com>
2025-08-11 19:11:54 +0200

    Changed email to required and type as well

--- auth_app/models.py
+++ auth_app/models.py
@@ -5,11 +5,14 @@ class CustomUser(AbstractUser):
     class Roles(models.TextChoices):
         CUSTOMER = "customer", "Customer"
         BUSINESS = "business", "Business"
-
+        
+    email = models.EmailField(unique=True)
     type = models.CharField(
         max_length=20,
         choices=Roles.choices,
         default=Roles.CUSTOMER,
+        blank=False,
+        null=False,
     )
 
     def __str__(self):
