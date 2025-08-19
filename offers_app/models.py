80d687ed2eb6f02ce8d60384e964f72f19004410
RichyClean <149680604+RichyClean@users.noreply.github.com>
2025-08-19 12:37:51 +0200

    created offer Model

--- offers_app/models.py
+++ offers_app/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from auth_app.models import CustomUser
 
 # Create your models here.
+
+class Offer(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    image = models.ImageField(upload_to='offer_images/', blank=True)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    min_price = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
+    min_delivery_time = models.IntegerField(blank=True, null=True)
+
+    class Meta:
+        verbose_name = "Offer"
+        verbose_name_plural = "Offers"
+        ordering = ['-created_at']
