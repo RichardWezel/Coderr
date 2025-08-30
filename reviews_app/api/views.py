d8ee077b1ed131a922de30bc73635c129bd92c8b
RichyClean <149680604+RichyClean@users.noreply.github.com>
2025-08-30 11:00:46 +0200

    added filter and ordering options to review

--- reviews_app/api/views.py
+++ reviews_app/api/views.py
@@ -2,10 +2,13 @@ from rest_framework import generics, permissions
 from reviews_app.models import Review
 from reviews_app.api.serializers import ReviewSerializer
 from reviews_app.api.permissions import OneReviewPerBusinessUserPermission
+from django_filters.rest_framework import DjangoFilterBackend
 
 class ReviewView(generics.ListCreateAPIView):
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly, OneReviewPerBusinessUserPermission]
-
+    ordering_fields = ['updated_at', 'rating']
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['business_user_id', 'reviewer_id']
     
