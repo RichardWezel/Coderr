3f2ac6f8c832024e1886e5b9d0c200fbf08ef8ba
Richard Wezel <richard.wezel@posteo.de>
2025-09-26 12:08:42 +0200

    layout

--- core/settings.py
+++ core/settings.py
@@ -13,12 +13,11 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 import os
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-# STATICFILES_DIRS = [BASE_DIR / 'static']      # nur falls du einen Quellordner hast
-STATIC_ROOT = BASE_DIR / 'staticfiles' 
 
+STATIC_ROOT = BASE_DIR / 'staticfiles' 
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
